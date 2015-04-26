ee1d88f5855066fb6d91e2e195de276b1f1ab501
Atom Raiff <araiff@yahoo-inc.com>
2015-04-25 19:46:28 -0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-var port = process.env.PORT || 5000;
-
-//setup express, mongo, and server vars
 var express = require("express"),
-    app = new express();    
+    app = express();    
 
+app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
-var fs = require('fs');
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.static(__dirname + '/views'));
-
-
-// Homepage
 app.get("/", function(req, res){	
   res.render("home");	
 });
 
-app.get("/rtpb", function(req,res){
-  res.render("rtpb");
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-app.listen(port);
